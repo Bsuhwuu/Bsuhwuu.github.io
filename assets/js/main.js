f5def7f507fdcd4484ffd38f39cc3938f6d6ba99
Bsuhwuu <ridhonovan951@gmail.com>
2024-10-28 20:18:34 +0700

    perbaikan responsiffff

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,17 @@
   const headerToggleBtn = document.querySelector('.header-toggle');
 
   function headerToggle() {
+    // Toggle kelas untuk menampilkan/menyembunyikan header
     document.querySelector('#header').classList.toggle('header-show');
-    headerToggleBtn.classList.toggle('bi-list');
-    headerToggleBtn.classList.toggle('bi-x');
-  }
-  headerToggleBtn.addEventListener('click', headerToggle);
+    
+    // Scroll halus ke header saat tombol toggle diklik
+    document.querySelector('#header').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Event listener untuk tombol toggle
+    headerToggleBtn.addEventListener('click', headerToggle);
+
+
 
   /**
    * Hide mobile nav on same-page/hash links
